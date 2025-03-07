a414cd79792956d690831ce695ffc9076187d3c2
Zhaina <123055378+lambheadz@users.noreply.github.com>
2025-03-07 09:14:56 +0500

    Update ContentView.swift

--- HeroRandomizer/HeroRandomizer/ContentView.swift
+++ HeroRandomizer/HeroRandomizer/ContentView.swift
@@ -15,18 +15,34 @@ struct ContentView: View {
             AsyncImage(url: viewModel.selectedHero?.imageUrl) { phase in
                 switch phase {
                 case .empty:
-                    Color.teal
-                        .frame(height: 300)
+                    Color.teal.frame(height: 300)
                 case .success(let image):
-                    image
-                        .resizable()
-                        .frame(height: 300)
-                case .failure(let error):
-                    Color.red
-                        .frame(height: 300)
+                    image.resizable().frame(height: 300).cornerRadius(15)
+                case .failure:
+                    Color.red.frame(height: 300)
                 }
             }
             .padding(32)
+
+            if let hero = viewModel.selectedHero {
+                VStack(alignment: .center, spacing: 8) {
+                    Text("Name: \(hero.name)")
+                        .font(.title).bold()
+                    Text("Gender: \(hero.appearance.gender)")
+                    Text("Race: \(hero.appearance.race ?? "Unknown")")
+                    Text("Height: \(hero.appearance.height.joined(separator: ", "))")
+                    Text("Weight: \(hero.appearance.weight.joined(separator: ", "))")
+                    Text("Intelligence: \(hero.powerstats.intelligence)")
+                    Text("Power: \(hero.powerstats.power)")
+                    Text("Strength: \(hero.powerstats.strength)")
+                    Text("Speed: \(hero.powerstats.speed)")
+                    Text("Durability: \(hero.powerstats.durability)")
+                    Text("Place of Birth: \(hero.biography.placeOfBirth)")
+                }
+                .padding()
+                .background(RoundedRectangle(cornerRadius: 10).fill(Color.gray.opacity(0.2)))
+                .padding(.horizontal)
+            }
 
             Spacer()
 
@@ -40,9 +56,8 @@ struct ContentView: View {
                     .foregroundColor(.white)
                     .padding()
                     .background(Color.blue)
-                    .cornerRadius(10)
+                    .cornerRadius(30)
             }
-
         }
     }
 }
